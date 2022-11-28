a730a5eda6c37ba461cf2e14201404e90f6425ef
balakumar <bala93kumar@gmail.com>
2022-11-28 23:54:20 +0530

    longestSubString

---
+++ com/balakumar/medium/LongestSubstringWithoutRepeatingBrute.java
@@ -0,0 +1,27 @@
+package com.balakumar.medium;
+
+public class LongestSubstringWithoutRepeatingBrute {
+
+    public static int longestSubString(String str){
+        int n = str.length();
+
+        return 1; 
+    }
+
+    public static void main(String[] args) {
+
+        boolean[] visited = new boolean[26];
+
+        for(int i = 0 ; i < visited.length ; i++) {
+            System.out.println(visited[i]);
+
+        String str = "geeksforgeeks";
+
+        int len  = longestSubString(str);
+
+        }
+
+
+
+    }
+}
